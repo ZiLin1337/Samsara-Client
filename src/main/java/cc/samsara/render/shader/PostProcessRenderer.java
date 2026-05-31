ceba41032a6b5ab58ee16e4893e5868cfc828eed
Samsara Dev <samsara@example.com>
2026-05-31 01:41:25 +0000

    fix: correct MatrixStack import path
    - Changed from net.minecraft.client.util.math.MatrixStack to com.mojang.math.MatrixStack

--- src/main/java/cc/samsara/render/shader/PostProcessRenderer.java
+++ src/main/java/cc/samsara/render/shader/PostProcessRenderer.java
@@ -17,7 +17,7 @@ import org.lwjgl.opengl.GL32C;
 
 import com.mojang.blaze3d.opengl.GlStateManager;
 
-import net.minecraft.client.util.math.MatrixStack;
+import com.mojang.math.MatrixStack;
 
 public class PostProcessRenderer {
 
